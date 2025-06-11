25336228b5f6cf88c7e13fdf71aed1e339d3960a
zm20142004 <zm.nashor@gmail.com>
2025-06-11 07:46:07 +0700

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const PORT = 80;
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Halo dari Web Server EC2!');
-}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
